33cc688d840d86b4e74fcf1350bc7726cf64ff18
George <g.e.tziolas@gmail.com>
2025-02-12 15:47:14 +0200

    Add "sensitivity" variable parsing.

--- batch_segmentation.py
+++ batch_segmentation.py
@@ -1,8 +1,7 @@
-from TheDuneAI import ContourPilot
+from segmentation_xai.TheDuneAI import ContourPilot
 import argparse
 
 def main():
-    # Argument parser
     parser = argparse.ArgumentParser(description='Run lung segmentation and xai image heatmap generation with Grad-CAM')
     parser.add_argument('--model_path', required=True,
                         help='Path to model directory containing model_v7.json and weights_v7.hdf5')
@@ -17,10 +16,10 @@ def main():
         model_path=args.model_path,
         data_path=args.path_to_test_data,
         output_path=args.save_path,
+        sensitivity=args.sensitivity,
         verbosity=True
     )
 
-    # Run segmentation/xai process
     # If a CUDA GPU is available, it is used automatically by tensorflow
     model.segment()
 
